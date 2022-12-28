384cb9b82e16f3636a510783943c2d279060a5dc
Bernardo Cajaseira <behsk8@gmail.com>
2022-12-28 19:25:59 -0300

    Update password-generator.py
    Adicionei um input para o usuário escolher o número de caracteres que ele deseja que a senha tenha

--- password-generator.py
+++ password-generator.py
@@ -1,9 +1,10 @@
 import string as st
 import numpy as np
 
+ncarac = int(input('Quantos caracteres quer sua senha? '))
 letras = st.ascii_letters
 numeros = st.digits
 especial = st.punctuation
 alg = (letras + numeros + especial)
-senha = np.random.choice(list(alg), 10)
+senha = np.random.choice(list(alg), ncarac)
 print(''.join(senha))
